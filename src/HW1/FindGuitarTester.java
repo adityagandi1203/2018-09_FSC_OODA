ee32279601cf602780a162fbfd2906e78fffa9d4
adityagandi1203 <43256465+adityagandi1203@users.noreply.github.com>
2018-09-26 01:21:02 -0400

    Update FindGuitarTester.java

--- src/HW1/FindGuitarTester.java
+++ src/HW1/FindGuitarTester.java
@@ -1,48 +1,80 @@
 package HW1;
+/**
+ * @subject:OOAD
+ * @author:orlando
+ * @version:1.1
+ * 01392300 Aditya
+ * @01392295 Nikhil
+ * @01390645 Raghunandan
+ */
+
+
+import java.util.Iterator;
+import java.util.List;
+
+
+
 public class FindGuitarTester {
 
-  public static void main(String[] args) {
-    // Set up Rick's guitar inventory
-    Inventory inventory = new Inventory();
-    initializeInventory(inventory);
+	public static void main(String[] args) {
+		// Set up Rick's guitar inventory
+		Inventory inventory = new Inventory();
+		initializeInventory(inventory);
+		GuitarSpec erinspec=new GuitarSpec(Manufacturer.Fender,"Stratocaster",Type.Electric,Wood.Alder,Wood.Alder);
+		Guitar whatErinLikes = new Guitar("", 1499.95, erinspec);
 
-    Guitar whatErinLikes = new Guitar("", 0, "fender", "Stratocastor", 
-                                      "electric", "Alder", "Alder");
-    Guitar guitar = inventory.search(whatErinLikes);
-    if (guitar != null) {
-      System.out.println("Erin, you might like this " +
-        guitar.getManufacturer() + " " + guitar.getModel() +  " " +
-        guitar.getType() + " guitar:\n   " +
-        guitar.getBackWood() + " back and sides,\n   " +
-        guitar.getTopWood() + " top.\nYou can have it for only $" +
-        guitar.getPrice() + "!");
-    } else {
-      System.out.println("Sorry, Erin, we have nothing for you.");
-    }
-  }
+		List listguitar=inventory.search(whatErinLikes);
 
-  private static void initializeInventory(Inventory inventory) {
-    inventory.addGuitar("11277", 3999.95, "Collings", "CJ", "acoustic",
-                        "Indian Rosewood", "Sitka");
-    inventory.addGuitar("V95693", 1499.95, "Fender", "Stratocastor", "electric",
-                        "Alder", "Alder");
-    inventory.addGuitar("V9512", 1549.95, "Fender", "Stratocastor", "electric",
-                        "Alder", "Alder");
-    inventory.addGuitar("122784", 5495.95, "Martin", "D-18", "acoustic",
-                        "Mahogany", "Adirondack");
-    inventory.addGuitar("76531", 6295.95, "Martin", "OM-28", "acoustic",
-                        "Brazilian Rosewood", "Adriondack");
-    inventory.addGuitar("70108276", 2295.95, "Gibson", "Les Paul", "electric",
-                        "Mahogany", "Maple");
-    inventory.addGuitar("82765501", 1890.95, "Gibson", "SG '61 Reissue",
-                        "electric", "Mahogany", "Mahogany");
-    inventory.addGuitar("77023", 6275.95, "Martin", "D-28", "acoustic",
-                        "Brazilian Rosewood", "Adirondack");
-    inventory.addGuitar("1092", 12995.95, "Olson", "SJ", "acoustic",
-                        "Indian Rosewood", "Cedar");
-    inventory.addGuitar("566-62", 8999.95, "Ryan", "Cathedral", "acoustic",
-                        "Cocobolo", "Cedar");
-    inventory.addGuitar("6 29584", 2100.95, "PRS", "Dave Navarro Signature",
-                        "electric", "Mahogany", "Maple");
-  }
-}
+		if (listguitar != null) 
+		{
+		
+			for(Iterator i=listguitar.iterator();i.hasNext();)
+			{
+				Guitar g=(Guitar)i.next();
+				System.out.println("Erin, you might like this " +
+						g.getGuitarSpec().getManufacturer() + " " + g.getGuitarSpec().getModel() +  " " +
+						g.getGuitarSpec().getType() + " guitar:\n   " +
+						g.getGuitarSpec().getbackwood() + " back and sides,\n   " +
+						g.getGuitarSpec().getTopwood() + " top.\nYou can have it for only $" +
+						g.getPrice() + "!");
+			}}
+			else 
+			{
+				System.out.println("Sorry, Erin, we have nothing for you.");
+			}
+		}
+		
+
+		private static void initializeInventory(Inventory inventory) {
+			inventory.addGuitar("11277", 3999.95, new GuitarSpec (Manufacturer.Collings, "CJ", Type.Acoustic,
+					Wood.IndianRosewood, Wood.Sitka));																//adding guitar with enums
+			inventory.addGuitar("V95693", 1499.95, new GuitarSpec (Manufacturer.Fender, "Stratocaster", Type.Electric,
+					Wood.Alder, Wood.Alder));
+			inventory.addGuitar("V95123", 1549.95, new GuitarSpec (Manufacturer.Fender, "Stratocaster", Type.Electric,
+					Wood.Alder, Wood.Alder));
+			inventory.addGuitar("122784", 5495.95, new GuitarSpec (Manufacturer.Martin, "D-18", Type.Acoustic,
+					Wood.Mahogany, Wood.Adirondack));
+			inventory.addGuitar("76531", 6295.95, new GuitarSpec (Manufacturer.Martin, "OM-28", Type.Acoustic,
+					Wood.IndianRosewood, Wood.Adirondack));
+			inventory.addGuitar("70108276", 2295.95, new GuitarSpec (Manufacturer.Gibson, "Les Paul", Type.Electric,
+					Wood.Mahogany, Wood.Maple));
+			inventory.addGuitar("82765501", 1890.95, new GuitarSpec (Manufacturer.Gibson, "SG '61 Reissue", Type.Electric,
+					Wood.Mahogany, Wood.Mahogany));
+			inventory.addGuitar("77023", 6275.95, new GuitarSpec (Manufacturer.Martin, "D-28", Type.Acoustic,
+					Wood.BrazilianRosewood, Wood.Adirondack));
+
+			inventory.addGuitar("1092",12995.95, new GuitarSpec (Manufacturer.Olson, "SJ", Type.Acoustic,
+					Wood.IndianRosewood, Wood.Cedar));
+			inventory.addGuitar("566-62",8999.95, new GuitarSpec (Manufacturer.Ryan, "Cathedral", Type.Acoustic,
+					Wood.Cocobolo, Wood.Cedar));
+			inventory.addGuitar("6 2958",2100.95, new GuitarSpec (Manufacturer.PRS, "Dave Navarro Signature", Type.Electric,
+					Wood.Mahogany, Wood.Maple));
+
+
+
+
+
+
+
+		}
+	}
