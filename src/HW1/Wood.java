0a34f11654a4879250254c85e5ca2373988013aa
adityagandi1203 <43256465+adityagandi1203@users.noreply.github.com>
2018-09-26 01:47:59 -0400

    Add files via upload

---
+++ src/HW1/Wood.java
@@ -0,0 +1,56 @@
+package HW1;
+/**
+ * @subject:OOAD
+ * @author:orlando
+ * @version:1.1
+ * 01392300 Aditya
+ * @01392295 Nikhil
+ * @01390645 Raghunandan
+ */
+
+
+public enum Wood {
+	
+	IndianRosewood,
+	BrazilianRosewood,
+	Mahogany,
+	Maple,
+	Cocobolo,
+	Cedar,
+	Adirondack,
+	Alder,
+	Sitka;
+	public String toString()
+	{
+		switch(this)
+		{
+		case IndianRosewood:
+			return "Indian Rosewood";
+		case BrazilianRosewood:
+			return "Brazilian Rosewood";
+		case Mahogany:
+			return "Mahogany";
+		case Maple:
+			return "Maple";
+		case Cocobolo:
+			return "Cocobolo";
+		case Cedar:
+			return "Cedar";
+		case Adirondack:
+			return "Adirondack";
+		case Alder:
+			return "Alder";
+		case Sitka:
+			return "Sitka";
+		default:throw new IllegalArgumentException();
+			
+			
+		
+		}
+		
+	}
+	
+	
+	
+
+}
